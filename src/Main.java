bc99da81a28123697a2fc34bf23a33b706c47e74
roandr1970 <ambal007a@yandex.ru>
2021-08-12 06:14:48 +0300

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 2000;
+        int costMile = 20;
+        int numberBonusMiles = priceTicket/costMile;
+        System.out.println(numberBonusMiles);
+    }
+}
